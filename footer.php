c357a61edac000f3905d34fffc60bd127861045e
Gianpaolo <39795391+GbaroneMpire@users.noreply.github.com>
2024-09-15 12:05:13 +0200

    footer update

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
   <div class="container">
 
     <?php if(is_active_sidebar('footer-widget-area')): ?>
-    <div class="row pt-5 pb-4 border-bottom" id="footer" role="navigation">
+    <div class="row pt-5 mb-4 border-bottom border-dark" id="footer" role="navigation">
       <?php dynamic_sidebar('footer-widget-area'); ?>
     </div>
     <?php endif; ?>
